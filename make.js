f965871b2647f673bc85809a7e8a4b3fc3046fb9
Owen Palmer <owen@lunatechnw.com>
2021-05-05 03:35:40 +0000

    moved the make 3d tri function to the correct file

--- make.js
+++ make.js
@@ -13,3 +13,12 @@ function mktriangle(x1, y1, x2, y2, x3, y3) {
     ];
     return triangle;
 }
+
+function mk3dtriangle(x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+    triangle = [
+        mkline(x1, y1, x2, y2),
+        mkline(x1, y1, x3, y3),
+        mkline(x3, y3, x2, y2),
+    ];
+    return triangle;
+}
